039434f676881ac06e00c69c0514d40bacc64760
racine fellipe <racine.3301@gmail.com>
2023-05-07 17:58:36 -0300

    recuperando entrada do form, armazenando as informações em lista e exibindo na pagina

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
 
-form.addEventListener("submit", (evento)=>{
+form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-
-    console.log(evento)
-    console.log(evento.target.elements['nome'].value)
-    console.log(evento.target.elements['quantidade'].value)
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
 })
+
+function criaElemento(nome, quantidade){
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const quantidadeItem = document.createElement('strong')
+    quantidadeItem.innerHTML = quantidade
+
+    novoItem.appendChild(quantidadeItem)
+    novoItem.innerHTML += nome
+    
+    lista.appendChild(novoItem)
+}
